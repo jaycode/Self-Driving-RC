6793b5a7f149d19853548470e7bef85de00862eb
jaycode <teguhwpurwanto@gmail.com>
2017-05-23 14:48:57 -0700

    fix: 2 inputs for engine motor

--- Arduino/v0.1/v0.1.ino
+++ Arduino/v0.1/v0.1.ino
@@ -3,23 +3,24 @@
 #include "car.h"
 #include "rc.h"
 
-uint8_t engineIN1Pin = 3;
-uint8_t engineIN2Pin = 9;
-uint8_t engineIN3Pin = 10;
-uint8_t engineIN4Pin = 11;
+uint8_t engineIN1Pin = 9;
+uint8_t engineIN2Pin = 6;
+//uint8_t engineIN3Pin = 10;
+//uint8_t engineIN4Pin = 11;
  
 uint8_t steerIN1Pin = 5; // 1A1
-uint8_t steerIN2Pin = 6; // 1B1
+uint8_t steerIN2Pin = 3; // 1B1
 const uint8_t steerFeedPin = A0;
 
-const uint8_t aux1Pin = 2;
-const uint8_t gearPin = 4;
-const uint8_t rudoPin = 7;
-const uint8_t elevPin = 8;
-const uint8_t ailePin = 12;
-const uint8_t throPin = 13;
+const uint8_t aux1Pin = 11;
+const uint8_t gearPin = 10;
+const uint8_t rudoPin = 8;
+const uint8_t elevPin = 7;
+const uint8_t ailePin = 4;
+const uint8_t throPin = 2;
 
-Motor engine(engineIN1Pin, engineIN2Pin, engineIN3Pin, engineIN4Pin);
+//Motor engine(engineIN1Pin, engineIN2Pin, engineIN3Pin, engineIN4Pin);
+Motor engine(engineIN1Pin, engineIN2Pin, NULL, NULL);
 SteeringWheel steer(steerIN1Pin, steerIN2Pin, steerFeedPin);
 RC rc(aux1Pin, gearPin, rudoPin, elevPin, ailePin, throPin);
   

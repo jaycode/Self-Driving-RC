e1701ef50bb47605e8636c94cf221607240fc496
Jaycode <teguhwpurwanto@gmail.com>
2017-06-03 19:16:39 -0700

    chore: Remove unneeded code

--- Computer/driver/main.py
+++ Computer/driver/main.py
@@ -111,35 +111,6 @@ def prepare_model(model_path):
 
     return load_model(model_path)
 
-def read_bytes_until(port, lchar, liter):
-    i = 0
-    value = ""
-    buff = b''
-    while buff != lchar and i < liter:
-        i+=1
-        # If an error happens caused by buff contains \xff or other
-        # UnicodeDEcodeError, that means it tries to read more data before
-        # it was available. Testing with the following code should help:
-        # x = port.read()
-        # x1 = port.read()
-        # x2 = port.read()
-        # x3 = port.read()
-        # x4 = port.read()
-        # x5 = port.read()
-        # print("debug:", (x+x1+x2+x3+x4+x5))
-        while port.in_waiting == 0: pass
-        value += str(buff, 'latin')
-        buff = port.read(1)
-    return value
-
-def read_bytes(port, n):
-    # This gives better output than port.read(n).
-    text = ""
-    for i in range(n):
-        text += str(port.read(), 'latin')
-    port.read() # Final newline
-    return text
-
 def read_status(port):
     status = {}
     value = ''
@@ -199,23 +170,6 @@ def main():
         print("single loop time:",loop_time-previous_time)
         previous_time = loop_time
 
-        # --- TESTING STEERING ---
-        # port.write(bytearray("{}{};".format(\
-        #     HOST_AUTO_STEER.decode(), "0250"), 'utf-8'))
-
-        # while port.in_waiting == 0: pass
-        # response = port.read()
-        # print("got byte:", time.time()-previous_time)
-        # print("response: {}".format(response))
-        # if response != b'':
-        #     print("connected")
-        #     is_connected = True
-
-        # print(read_bytes(port, 6))
-        # --- END TESTING STEERING ---
-
-
-        # if mode == MODE_NONE:
         port.write(HOST_REQUEST_UPDATE)
         # non-blocking check whether device sends BEGIN
         begin = port.read(1)
@@ -290,99 +244,6 @@ def main():
                             HOST_AUTO_THROTTLE.decode(), str(throttle)), 'utf-8'))
                         port.write(bytearray("{}{};".format(\
                             HOST_AUTO_STEER.decode(), str(new_steer)), 'utf-8'))
-
-
-        # response = b'';
-        # if not is_connected:
-        #     response = port.read()
-        #     print("response: {}".format(response))
-        #     if response != b'':
-        #         print("connected")
-        #         is_connected = True
-
-        # update = str(response, 'latin') + read_bytes(port, 14)
-        # if update == '': continue
-
-        # if update[0].encode('latin') == DEV_BEGIN:
-        #     command = update[1].encode('latin')
-
-        # print("update: {}".format(update))
-
-        # if command == DEV_DEBUG:
-        #     print("debug:", update[2:])
-        # elif command == DEV_STATUS:
-        #     regex = r'm([0-9])v([-+]?[0-9]*\.?[0-9]+)s([-+]?[0-9]+)'
-        #     matches = re.findall(regex, update[2:])[0]
-        #     status = {'mode': int(matches[0]),
-        #               'speed': float(matches[1]),
-        #               'steer': int(matches[2])}
-        #     print("status: mode {} speed {} steer {}".format(
-        #         status['mode'], status['speed'], status['steer']))
-
-        #     if status['mode'] == DRIVE_MODE_RECORDED:
-        #         # Record this frame.
-        #         # No need to do image preprocessing here. We want the
-        #         # raw image and experiment with preprocessing later in
-        #         # training phase. The final preprocessing will then
-        #         # be implemented in the inference phase.
-        #         ret, frame = cams[0].read()
-
-        #         # Create image path.
-        #         filename = "{}.jpg".format(tstamp)
-        #         path = os.path.join(RECORDED_IMG_PATH, filename)
-
-        #         # We put the makedirs here to ensure directory is created
-        #         # when re-recording without having to reset the script.
-        #         os.makedirs(RECORDED_IMG_PATH, exist_ok=True)
-
-        #         # Save image
-        #         cv2.imwrite(path, frame)
-
-        #         # Append to training data.
-        #         if not os.path.isfile(RECORDED_CSV_PATH):
-        #             fd = open(RECORDED_CSV_PATH, 'w')
-        #             head = "filename, steer, speed\n"
-        #             fd.write(head)
-        #         else:
-        #             fd = open(RECORDED_CSV_PATH,'a')
-        #         row = "{}, {}, {}\n".format(filename, status['steer'], status['speed'])
-        #         fd.write(row)
-        #         fd.close()
-        #     elif status['mode'] == DRIVE_MODE_AUTO:
-        #         # Inference phase
-                
-        #         # Read image and do image preprocessing (when needed)
-        #         ret, frame = cams[0].read()
-
-        #         image_array = np.asarray(frame)
-        #         throttle = controller.update(status['speed'])
-        #         msg = None
-        #         try:
-        #             prediction = model.predict(\
-        #                 image_array[None, :, :, :], batch_size=1)
-        #             new_steer = prediction[0][0]
-        #         except TypeError as err:
-        #             msg = "TypeError: {}".format(err)
-        #             print(msg)
-        #         except ValueError as err:
-        #             msg = "TypeError: {}".format(err)
-        #             print(msg)
-        #         except UnboundLocalError as err:
-        #             # No model since auto mode was disabled.
-        #             msg = "AUTO mode was disabled since no model was initialized."
-        #             print(msg)
-        #         if msg:
-        #             with open('error.log','a') as f:
-        #                 f.write(msg)
-        #                 f.write("\n")
-        #         else:
-        #             throttle = int(throttle)
-        #             new_steer = int(new_steer)
-        #             print("throttle: {} steer: {}".format(throttle, new_steer))
-        #             port.write(bytearray("{}{};".format(\
-        #                 HOST_AUTO_THROTTLE.decode(), str(throttle)), 'utf-8'))
-        #             port.write(bytearray("{}{};".format(\
-        #                 HOST_AUTO_STEER.decode(), str(new_steer)), 'utf-8'))
 
 if __name__ == "__main__":
     main()
